7c8176700198dda5c3223a32cd8c553e674cfc71
kaajavi <javierguignard@gmail.com>
2016-05-22 20:22:18 -0300

    Uso de forms

--- inscripciones/urls.py
+++ inscripciones/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.views.generic.edit import UpdateView
 
 urlpatterns = patterns('',
-                       url(r'^',
-                          'inscripciones.views.visor',
-                          name='visor'),
+                       url(r'^$',
+                          'inscripciones.views.abm_alumno',
+                          name='alta_alumno'),
+                       url(r'^edit/(?P<id_alumno>\d+)/$',
+                          'inscripciones.views.abm_alumno',
+                          name='edit_alumno'),
                        url(r'^foo',
                            'inscripciones.views.home',
                            name='home'),
--- inscripciones/views.py
+++ inscripciones/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, render, redirect
+from django.core.urlresolvers import reverse
 from django.template import RequestContext
 # Create your views here.
 from django.contrib.auth.decorators import login_required
@@ -12,6 +13,30 @@ def home(request):
     context = RequestContext(request)
     context['cursos'] = Curso.objects.all()
     return render_to_response('index.html',context)
+
+def abm_alumno(request, id_alumno=None):
+    '''
+    Si pasas un id, editas, si no, creas uno nuevo
+    '''
+    context = RequestContext(request)
+    if (id_alumno!=None):
+        alumno = Alumno.objects.get(pk=id_alumno)
+    else:
+        alumno = Alumno()
+
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST, instance=alumno)
+        if form.is_valid():
+            alumno = form.save(commit=False)
+            alumno.save()
+            rev = reverse('home:edit_alumno', args=[alumno.id])
+            return redirect(rev)
+    else:
+        form=AlumnoForm(instance=alumno)
+    context['form']=form
+    return render(request, 'abm_alumno.html',context)
+
+
 
 def visor(request):
     form = []
